2c26fc93806694f7062f02535f9efc12518c2f7e
maheshwaribittla <maheshwari.bittla@gmail.com>
2021-05-21 06:58:01 +0530

    modified pom and basetest

--- src/test/java/com/base/TestBase.java
+++ src/test/java/com/base/TestBase.java
@@ -55,7 +55,7 @@ public class TestBase {
 			try {
 				config.load(fis);
 
-				log.info("loaded the config properties file");
+				log.info("loaded the config properties file successfully");
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -74,7 +74,7 @@ public class TestBase {
 			try {
 				or.load(fis);
 
-				log.info("loaded the or file");
+				log.info("loaded the or file successfully");
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
